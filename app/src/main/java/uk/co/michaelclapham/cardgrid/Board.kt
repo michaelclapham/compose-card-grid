bfe2387e202b17c48bf50cd08075421dc7c29dc7
Michael Clapham <michaeljclapham@gmail.com>
2022-07-22 13:41:14 +0100

    Use full width and add rounded corners

--- app/src/main/java/uk/co/michaelclapham/cardgrid/Board.kt
+++ app/src/main/java/uk/co/michaelclapham/cardgrid/Board.kt
@@ -1,36 +1,53 @@
 package uk.co.michaelclapham.cardgrid
 
+import java.lang.Exception
 import kotlin.random.Random
 
 class Board(val width: Int, val height: Int, numSpy: Int, numAssassin: Int) {
 
-    private var grid: Array<Array<Card>> = Array(height) { _ -> Array(width) { _ -> Card.NEUTRAL } }
+    private var grid: Array<Array<CardType>> = Array(height) { _ -> Array(width) { _ -> CardType.NEUTRAL } }
 
     init {
-        val deck = (0..(width * height)).mapIndexed { index, _ -> index }.toMutableList()
+        val deck = (0 until(width * height)).mapIndexed { index, _ -> index }.toMutableList()
         repeat(numSpy) {
-            drawFromDeck(deck, Card.SPY)
+            drawFromDeck(deck, CardType.SPY)
         }
         repeat(numAssassin) {
-            drawFromDeck(deck, Card.ASSASSIN)
+            drawFromDeck(deck, CardType.ASSASSIN)
         }
     }
 
-    private fun drawFromDeck(deck: MutableList<Int>, card: Card) {
-        val rand = Random.nextInt(deck.size)
-        val randIndex = deck.removeAt(rand)
-        val x = randIndex / width
-        val y = randIndex % height
-        grid[x][y] = card
+    private fun drawFromDeck(deck: MutableList<Int>, card: CardType) {
+        var rand = Random.nextInt(deck.size)
+        var randIndex = -1
+        var x = -1
+        var y = -1
+        try {
+            rand = Random.nextInt(deck.size)
+            randIndex = deck.removeAt(rand)
+            x = randIndex / width
+            y = randIndex % height
+            grid[x][y] = card
+        } catch (ex: Exception) {
+            println("Draw from deck exception $ex")
+            // do nothing
+        }
+
     }
 
-    fun getAtIndex(x: Int, y: Int): Card {
-        return grid[y][x]
+    fun getAtIndex(x: Int, y: Int): CardType {
+        try {
+            return grid[x][y]
+        } catch (ex: Exception) {
+            println("ex $ex")
+            return CardType.NEUTRAL
+        }
+
     }
 
 }
 
-enum class Card {
+enum class CardType {
     SPY,
     ASSASSIN,
     NEUTRAL
--- app/src/main/java/uk/co/michaelclapham/cardgrid/MainActivity.kt
+++ app/src/main/java/uk/co/michaelclapham/cardgrid/MainActivity.kt
@@ -6,19 +6,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import uk.co.michaelclapham.cardgrid.ui.theme.AssassinCardColor
 import uk.co.michaelclapham.cardgrid.ui.theme.CardGridTheme
 import uk.co.michaelclapham.cardgrid.ui.theme.SpyCardColor
 import uk.co.michaelclapham.cardgrid.ui.theme.NeutralCardColor
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,23 +42,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-fun randomColor(): Color {
-    val random = Random.nextFloat()
-    return if (random < 0.1) {
-        AssassinCardColor
-    } else if (random < 0.5) {
-        SpyCardColor
-    } else {
-        NeutralCardColor
+fun colorForCard(card: CardType): Color {
+    return when (card) {
+        CardType.NEUTRAL -> NeutralCardColor
+        CardType.SPY -> SpyCardColor
+        CardType.ASSASSIN -> AssassinCardColor
     }
 }
 
-fun colorForCard(card: Card): Color {
-    return when (card) {
-        Card.NEUTRAL -> NeutralCardColor
-        Card.SPY -> SpyCardColor
-        Card.ASSASSIN -> AssassinCardColor
-    }
+@Composable
+fun RoundedCornerShapeDemo(){
+    ExampleBox(shape = RoundedCornerShape(5.dp))
+}
+
+@Composable
+fun ExampleBox(shape: Shape){
+    Box(
+        modifier = Modifier.size(40.dp).clip(shape).background(Color.Red)
+    )
 }
 
 @Composable
@@ -63,21 +68,28 @@ fun CardGrid(board: Board) {
     Column(Modifier.fillMaxWidth()) {
         Text("width = ${board.width}, height = ${board.height}")
         repeat(board.width) { y ->
-            Row() {
+            Row(Modifier.fillMaxWidth()) {
                 repeat(board.height) { x ->
-                    Column() {
-                        val card = board.getAtIndex(x, y)
-                        Text(
-                            text = "($x, $y)",
-                            color = Color.Black,
-                            fontSize = 30.sp,
-                            modifier = Modifier.background(colorForCard(card))
-                        )
-                    }
+                        val cardType = board.getAtIndex(x, y)
+                        GameCard(cardType)
+                        // RoundedCornerShapeDemo()
                 }
             }
         }
     }
+}
+
+@Composable
+fun RowScope.GameCard(cardType: CardType) {
+    Box(
+        modifier = Modifier
+            .weight(1f)
+            .height(40.dp)
+
+            .clip(RoundedCornerShape(10.dp))
+            .background(colorForCard(cardType))
+    )
+
 }
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES,
